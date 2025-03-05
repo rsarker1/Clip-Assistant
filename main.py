5d4b5bc70a0b0c825964a48b2b4b488ddb00d7fc
rsarker1 <rahul.sarker@stonybrook.edu>
2025-03-05 01:17:37 -0500

    Yaml and unit test setup

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import vosk
 import simpleobsws
 import asyncio
+import yaml
 
 
 async def main():
     print('test')
 
+def generate_config():
+    print('Generate')
 
 
 asyncio.run(main())
+
+# Generate config.yaml if it doesn't exist
