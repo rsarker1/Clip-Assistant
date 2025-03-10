7b983f84fd27cd64e80df7e7dac23147f8e8e601
rsarker1 <rahul.sarker@stonybrook.edu>
2025-03-09 21:09:21 -0400

    Setting up YAML generator

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-import vosk
-import simpleobsws
+import os
 import asyncio
 import yaml
+import simpleobsws
+import vosk
+
+def set_config_values():
+    user_config = {
+        'host': 'localhost',
+        'port': 4455
+    }
+    
+    user_config['host'] = prompt_user('Enter the host address: ', user_config['host'])
+    
+    while True:
+        port = prompt_user('Enter the specified port: ', user_config['port'])
+        try:
+            user_config['port'] = int(port)
+            break
+        except ValueError:
+            print('ERROR: Unexpected data type received. Try again.')
+    
+    while True: 
+        password = prompt_user('Enter the password (Found under Tools->Websocket Server Settings->Server Password): ')  
+        if not password:
+            print('ERROR: A password is required. Try again.')
+        else:
+            user_config['password'] = password
+            break
+        
+    return user_config
+    
+def prompt_user(text, default_value=None):
+    user_def = input(text)
+    if not user_def:
+        user_def = default_value
+    return user_def
 
 
-async def main():
-    print('test')
+def load_config(file_name='config.yaml'):
+    print('Checking if config file exists...')
+    if os.path.exists(file_name):
+        print('Config found')
+        with open(file_name, 'r') as file:
+            config = yaml.safe_load(file)
+    else:
+        print('Config does not exist. Generating...')
+        config = set_config_values()
+        with open(file_name, 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
+    print('Config loaded')
+    
+    
+    
+    return
 
-def generate_config():
-    print('Generate')
+def main():
+    load_config()
+    
 
-
-asyncio.run(main())
+main()
 
 # Generate config.yaml if it doesn't exist
