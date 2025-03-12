3615d5e4aeabcd1f6b0a809a6cf77ef9a6e3bae4
rsarker1 <rahul.sarker@stonybrook.edu>
2025-03-12 19:33:53 -0400

    Added timestamps, cleaned logs, and added dating to log name

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import asyncio
 import logging
+from datetime import datetime
 from yaml_config import load_config
 from obs_controller import OBSRecordingController
 from voice_recognizer import VoskVoiceRecognizer
 
 async def main():
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='clip_assistant.log', filemode='w', level=logging.INFO)
+    current_date = datetime.today().strftime('%Y_%m_%d')
+    logging.basicConfig(
+        filename=f'clip_assistant_{current_date}.log', 
+        filemode='w', 
+        format='%(asctime)s:[%(levelname)s]:(%(name)s):%(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.INFO
+    )
     logger.info('Started')
     
     config = load_config()
